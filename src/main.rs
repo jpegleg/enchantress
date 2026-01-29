f24a9ce98550277d03ad5076b55f22c67ed03185
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-01-29 12:11:46 -0500

    version printing update

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     }
     let input_file = &args[1];
     if input_file == "-v" {
-      println!("{{\"Version\": \"0.1.10\"}}");
+      println!("{{\"Version\": \"0.1.11\"}}");
       process::exit(0);
     }
     if args.len() != 4 {
